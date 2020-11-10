08c5cefff46ed810c6279052e63344c29fd27cf3
LarsOHelaas <70975771+LarsOHelaas@users.noreply.github.com>
2020-11-10 02:21:29 +0100

    Version 1.0.5
    Brukere kan ikke opprette bruker med eksisterende brukernavn

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { Router } = require('express');
+//const { Router } = require('express');
 const secureEndpoints = require('./modules/secureEndpoints');
 const user = require('./modules/user')
+
 
 
 
@@ -11,6 +12,7 @@ const server = express();
 server.set('port', (process.env.PORT || 8080));
 server.use(express.static('public'));
 server.use(bodyParser.json());
+
 
 server.listen(server.get('port'), function () {
     console.log('server running', server.get('port'));
@@ -22,5 +24,10 @@ server.use("/secure", secureEndpoints);
 server.post("/user", async function (req, res){
     const newUser = new user(req.body.username, req.body.password);
     await newUser.create();
+    res.status(200).json(newUser).end();
+});
+
+server.post("/user/login", async function (req, res){
+
     res.status(200).json(newUser).end();
 });
